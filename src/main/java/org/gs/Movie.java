5f39cc89f35e7b84d5d964254f404f22cf5e23a2
AnOnRT <arturpapyan1995@gmail.com>
2022-09-07 17:35:39 +0400

    1st commit

---
+++ src/main/java/org/gs/Movie.java
@@ -0,0 +1,56 @@
+package org.gs;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 100)
+    private String title;
+
+    @Column(length = 200)
+    private String description;
+
+    private String director;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
